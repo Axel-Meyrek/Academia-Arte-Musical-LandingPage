b7fc809bbd156afd0c55ea117f7aef4399a0e6f2
Axel Meyrek <chavezsud1406@gmail.com>
2024-09-22 16:00:58 -0600

    Add description Convenios

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const $navFlotante = document.querySelector('#navFlotante');
 
 const $buttonMenu = document.querySelector('#buttonMenu');
 
+const $convenioTEB = document.querySelector('#convenioTEB');
+
+const $convenioMAP = document.querySelector('#convenioMAP');
+
+const $buttonConvenioMAP = document.querySelector('#buttonConvenioMAP');
+
+const $buttonConvenioTEB = document.querySelector('#buttonConvenioTEB');
+
 
 
 //FUNCIONES
@@ -10,7 +18,25 @@ const showMenu = () => {
     $navFlotante.classList.toggle('showMenu');
 }
 
+const showConvenioTEB = () => {
+    $convenioTEB.classList.add('showConvenio');
+}
+
+const showConvenioMAP = () => {
+    $convenioMAP.classList.add('showConvenio');
+}
+
+const closeConvenio = () => {
+    $convenioMAP.classList.remove('showConvenio');
+    
+    $convenioTEB.classList.remove('showConvenio');
+}
+
 
 
 //EVENTOS
 $buttonMenu.addEventListener('click', showMenu);
+
+$buttonConvenioMAP.addEventListener('click', showConvenioMAP);
+
+$buttonConvenioTEB.addEventListener('click', showConvenioTEB);
