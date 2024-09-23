a7a082d3c61e42a32a08ba6066528a46ad7b3593
Axel Meyrek <chavezsud1406@gmail.com>
2024-09-22 21:14:55 -0600

    Add function of SaveUser

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ const $buttonConvenioMAP = document.querySelector('#buttonConvenioMAP');
 
 const $buttonConvenioTEB = document.querySelector('#buttonConvenioTEB');
 
+const $buttonFormulario = document.querySelector('#buttonFormulario');
+
+const $nombreCompleto = document.querySelector('#nombreCompleto');
+
+const $cursoInteresado = document.querySelector('#cursoInteresado');
+
+const $correoElectronico = document.querySelector('#correoElectronico');
+
+const $numeroCelular = document.querySelector('#numeroCelular');
+
+const $estadoLocalidad = document.querySelector('#estadoLocalidad');
 
 
 //FUNCIONES
@@ -28,8 +39,47 @@ const showConvenioMAP = () => {
 
 const closeConvenio = () => {
     $convenioMAP.classList.remove('showConvenio');
-    
+
     $convenioTEB.classList.remove('showConvenio');
+}
+
+const saveUser = (e) => {
+    e.preventDefault();
+
+    if($nombreCompleto.value == '' ||
+        $cursoInteresado.value == '' ||
+        $correoElectronico.value == '' ||
+        $numeroCelular.value == '' ||
+        $estadoLocalidad.value == ''
+    ){
+        return;
+    }
+
+
+    fetch("https://sheet.best/api/sheets/93d24933-8e35-497e-9b6f-4ed9c8570123", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            "NOMBRE_COMPLETO": $nombreCompleto.value,
+            "CURSO_INTERESADO": $cursoInteresado.value,
+            "CORREO_ELECTRONICO": $correoElectronico.value,
+            "NUMERO_CELULAR": $numeroCelular.value,
+            "ESTADO_LOCALIDAD": $estadoLocalidad.value
+        }),
+    })
+
+    cleanInputs();
+}
+
+const cleanInputs = () => {
+    $nombreCompleto.value = '';
+    $cursoInteresado.value = '';
+    $correoElectronico.value = '';
+    $numeroCelular.value = '';
+    $estadoLocalidad.value = '';
 }
 
 
@@ -40,3 +90,5 @@ $buttonMenu.addEventListener('click', showMenu);
 $buttonConvenioMAP.addEventListener('click', showConvenioMAP);
 
 $buttonConvenioTEB.addEventListener('click', showConvenioTEB);
+
+$buttonFormulario.addEventListener('click', saveUser);
